e12a7352ed25949310047efc84a286261fe632c8
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-01-26 11:40:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.json())
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/api/admin/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/admin/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 app.use("/api/admin", userRouter);
 
