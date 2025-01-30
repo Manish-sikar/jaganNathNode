0b3a737584816acb758c24fd59183f1b2e43668c
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-01-31 00:05:23 +0530

    Update footerModel.js

--- v1/models/footerModel.js
+++ v1/models/footerModel.js
@@ -1,53 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const FooterSchema = new mongoose.Schema({
-  footer_title: {
-    type: String,
-    required: true
-  },
-  footer_desc: {
-    type: String,
-    required: true
-  },
-  footer_social_details: [{
-    icon_name: {
+const FooterSchema = new mongoose.Schema(
+  {
+    footer_title: {
       type: String,
-      required: true
+      required: true,
     },
-    icon_url: {
+    footer_desc: {
       type: String,
-      required: true
+      required: true,
     },
-    icon_class: {
+    footer_social_details: [
+      {
+        icon_name: {
+          type: String,
+          required: true,
+        },
+        icon_url: {
+          type: String,
+          required: true,
+        },
+        icon_class: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    footer_our_services: [
+      {
+        service_name: { type: String, required: true },
+      },
+    ],
+    footer_other_services: [
+      {
+        service_name: { type: String, required: true },
+      },
+    ],
+    footer_banking_services: [
+      {
+        service_name: { type: String, required: true },
+      },
+    ],
+
+    footer_address1: {
       type: String,
-      required: true
-    }
-  }],
-  footer_services: [{
-    service_name: {
+      required: true,
+    },
+    footer_address2: {
       type: String,
-      required: true
-    }
-  }],
-  footer_address1: {
-    type: String,
-    required: true
+      required: true,
+    },
+    footer_email: {
+      type: String,
+      required: true,
+    },
   },
-  footer_address2: {
-    type: String,
-    required: true
-  },
-  footer_email: {
-    type: String,
-    required: true
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
   }
-
-  
-}, {
-  timestamps: true // Automatically adds createdAt and updatedAt fields
-});
+);
 
 // Export the schema as a model
-const FooterModel = mongoose.model('footer_detail', FooterSchema);
+const FooterModel = mongoose.model("footer_detail", FooterSchema);
 
 module.exports = FooterModel;
