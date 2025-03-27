e794388f76e3f50bcae5273954fd9d6b9ec5ca8a
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-03-27 20:31:38 +0530

    Update applyFormController.js

--- v1/controller/applyFormController.js
+++ v1/controller/applyFormController.js
@@ -72,7 +72,7 @@ const partnerData = await Partner.findOne({ JN_Id: partnerEmail });
     const uploadFileToS3 = async (file, folder) => {
       if (!file) return null; // If no file is provided, return null
 
-      const uniqueFilename = ${folder}/${Date.now()}_${uuidv4()}_${file.originalname};
+      const uniqueFilename = `${folder}/${Date.now()}_${uuidv4()}_${file.originalname}`;
       const uploadParams = {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: uniqueFilename,
@@ -132,7 +132,7 @@ const partnerData = await Partner.findOne({ JN_Id: partnerEmail });
       JN_Id: partnerEmail,
       amountDeducted: amount,
       availableBalanceAfter: updatedBalance,
-      purpose: Applied for ${subCategory} under ${category} category.,
+      purpose: `Applied for ${subCategory} under ${category} category.`,
     });
 
     await transaction.save(); // Save to DB
@@ -146,10 +146,33 @@ const partnerData = await Partner.findOne({ JN_Id: partnerEmail });
     return res
       .status(500)
       .json({ err: "An error occurred while processing the request." });
-  }
+}
 };
 
+const getUserApplyForm = async (req, res) => {
+  try {
+    // Retrieve all contact form details
+    const userFormDetails = await UserApplyFormModel.find();
 
+    // Check if there are no records in the database
+    if (userFormDetails.length === 0) {
+      return res.status(404).json({
+        error: "No contact form details found.",
+      });
+    }
+
+    // Return the retrieved data with a success message
+    return res.status(200).json({
+      userForm_Data: userFormDetails,
+      message: "User Apply form details retrieved successfully!",
+    });
+  } catch (error) {
+    console.error("Error retrieving User Apply form details:", error);
+    return res.status(500).json({
+      error: "An internal server error occurred.",
+    });
+  }
+};
 
 
 
