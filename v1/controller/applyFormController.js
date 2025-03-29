c4da28951f093d8bae41182343cf6683ff660034
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-03-29 09:11:33 +0530

    Update applyFormController.js

--- v1/controller/applyFormController.js
+++ v1/controller/applyFormController.js
@@ -148,36 +148,8 @@ const partnerData = await Partner.findOne({ JN_Id: partnerEmail });
       .json({ err: "An error occurred while processing the request." });
 }
 };
+ 
 
-const getUserApplyForm = async (req, res) => {
-  try {
-    // Retrieve all contact form details
-    const userFormDetails = await UserApplyFormModel.find();
-
-    // Check if there are no records in the database
-    if (userFormDetails.length === 0) {
-      return res.status(404).json({
-        error: "No contact form details found.",
-      });
-    }
-
-    // Return the retrieved data with a success message
-    return res.status(200).json({
-      userForm_Data: userFormDetails,
-      message: "User Apply form details retrieved successfully!",
-    });
-  } catch (error) {
-    console.error("Error retrieving User Apply form details:", error);
-    return res.status(500).json({
-      error: "An internal server error occurred.",
-    });
-  }
-};
-
-
-
-
-  
 const getUserApplyForm = async (req, res) => {
   try {
     // Retrieve all contact form details
