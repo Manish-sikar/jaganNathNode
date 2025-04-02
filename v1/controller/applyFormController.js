7155ca393027d9bc40cde2490de695e91c3fd441
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-04-02 22:22:49 +0530

    Update applyFormController.js

--- v1/controller/applyFormController.js
+++ v1/controller/applyFormController.js
@@ -97,12 +97,11 @@ const partnerData = await Partner.findOne({ JN_Id: partnerEmail });
       return crypto.randomInt(100000, 999999).toString(); // Generates a 6-digit random number
     };
     const Token_NO = generateToken();
-    
-// UserApplyFormModel.collection.dropIndex("email_1").then(() => {
-//   console.log("Unique index on email dropped successfully!");
-// }).catch(err => {
-//   console.log("Error dropping index:", err);
-// });
+     UserApplyFormModel.collection.dropIndex("email_1").then(() => {
+ console.log("Unique index on email dropped successfully!");
+}).catch(err => {
+ console.log("Error dropping index:", err);
+});
 
 
     
